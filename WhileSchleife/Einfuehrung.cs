f3555625ae6f99575a9e7ce5cc2fa5379a567483
Jan Suchotzki <jan@lernmoment.de>
2016-01-27 11:21:49 +0100

    Fügt ein Beispiel für return ein und eine Beschreibung was eine Iteration ist.

--- WhileSchleife/Einfuehrung.cs
+++ WhileSchleife/Einfuehrung.cs
@@ -6,11 +6,18 @@ namespace WhileSchleife
     {
         static void Main(string[] args)
         {
+            // Beschreibung(); -> Endlosschleife! Diese Methode nicht verwenden, sondern nur anschauen!
+
             SchleifeWirdEinmalAusgefuehrt();
             TrennerAusgeben();
+
             SchleifenrumpfWirdNichtAusgefuehrt();
             TrennerAusgeben();
+
             Break_SchleifeVorzeitigVerlassen();
+            TrennerAusgeben();
+
+            Return_VerlaesstAuchDieSchleife();
             TrennerAusgeben();
 
             Console.WriteLine("Zum beenden bitte RETURN drücken!");
@@ -35,6 +42,12 @@ namespace WhileSchleife
                 // mehr erfüllt ist. Dabei wird die Bedingung VOR jeder Ausführung des 
                 // Schleifenrumpfs abgefragt.
             }
+
+            // ITERATION: Das einmalige Ausführen des Schleifenrumpfes wird als Iteration bezeichnet.
+            // Bei der while-Schleife gibt es also sowohl Iterationen, bis die Bedingung nicht
+            // mehr TRUE ist. Das bedeutet, dass es je nach Bedingung die Anzahl der möglichen
+            // Iterationen zwischen 0 (Bedingung ist bei der ersten Überprüfung FALSE) und Unendlich
+            // (Bedingung ist immer TRUE) liegt.
 
             // ENDLOS-SCHLEIFE: wenn du diese Methode ausführen würdest, würde das Program nie
             // beendet werden. Du könntest es nur noch im Debugger oder über den TaskManager beenden!
@@ -100,6 +113,35 @@ namespace WhileSchleife
             // HINWEIS: Die Verwendung von BREAK ist häufig sehr verwirrend. Daher solltest du dir
             // gut überlegen, ob du dies Schlüsselwort wirklich verwenden willst/musst. Es ist häufig
             // ein Zeichen, dass die Schleife falsch eingesetzt wird!
+        }
+
+        static void Return_VerlaesstAuchDieSchleife()
+        {
+            // Schlüsselwort: return
+
+            // Wenn du return im Rumpf einer while-Schleife verwendest, wird nicht nur die Methode
+            // verlassen, in der deine while-Schleife ist, sondern ebenfalls die Schleife.
+            // Dabei wird der Schleifenrumpf sofort verlassen. Es wird also nicht nochmals die
+            // Bedingung überprüft oder noch weitere Befehle im Schleifenrumpf ausgeführt.
+
+            Console.WriteLine("Return: while-Schleife wird gestartet!");
+
+            int zaehler = 0;
+            while (true)
+            {
+                if (zaehler > 3)
+                {
+                    Console.WriteLine("Methode und Schleife werden jetzt sofort verlassen. Bedingung ist immer noch TRUE!");
+                    return;
+                }
+
+                zaehler = zaehler + 1;
+                Console.WriteLine("Schleifenrumpf wird ausgeführt. Zaehler hat den Wert: {0}", zaehler);
+            }
+
+            // HINWEIS: Auch die Verwendung von RETURN kann verwirrend sein, weil man es im Quell-
+            // code schnell mal überlesen kann. Überlege lieber, ob du deine Schleife nicht so 
+            // erstellen kannst, dass du return erst nach der Schleife verwendest!
         }
 
         static void TrennerAusgeben()
