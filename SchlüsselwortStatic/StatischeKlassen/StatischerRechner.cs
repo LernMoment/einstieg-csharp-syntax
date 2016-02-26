5d8a08eea090bfa26a604cb9e30babb171e1e007
Jan Suchotzki <jan@lernmoment.de>
2016-02-26 19:20:54 +0100

    Zeigt wie eine statische Klasse erstellt werden kann anhand eines Beispiels.

---
+++ SchlüsselwortStatic/StatischeKlassen/StatischerRechner.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SchlüsselwortStatic.StatischeKlassen
+{
+    public static class StatischerRechner
+    {
+        public static int Addieren(int ersterSummand, int zweiterSummand)
+        {
+            return ersterSummand + zweiterSummand;
+        }
+    }
+}
