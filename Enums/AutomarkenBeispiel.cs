9135ed88ff005f65faab615840c911e045e09efc
Jan Suchotzki <jan@lernmoment.de>
2016-07-07 17:20:52 +0200

    Fügt Beispiel ohne Enums hinzu

---
+++ Enums/AutomarkenBeispiel.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Enums
+{
+
+    public class AutomarkenBeispiel
+    {
+        const int porsche = 1;
+        const int audi = 2;
+
+        public static void Ausfuehren()
+        {
+            GibAutoMarkeAus(porsche);
+        }
+
+        private static void GibAutoMarkeAus(int marke)
+        {
+            string ausgabe;
+
+            switch (marke)
+            {
+                case porsche:
+                    ausgabe = "Du fährst ein Auto der Marke: Porsche";
+                    break;
+                case audi:
+                    ausgabe = "Du fährst ein Auto der Marke: Audi";
+                    break;
+                default:
+                    ausgabe = "Deine Automarke kenne ich nicht";
+                    break;
+            }
+
+            Console.WriteLine(ausgabe);
+        }
+
+        private static void Beschleunige(string autoMarke)
+        {
+            if (autoMarke == "Audi")
+            {
+                Console.WriteLine("Dein Audi beschleunigt so langsam auf 100km/h.");
+            }
+            else if (autoMarke == "Porsche")
+            {
+                Console.WriteLine("Dein Porsche rast auf 100km/h");
+            }
+            else
+            {
+                Console.WriteLine("Deine mir nicht bekannte Automarke wird auch irgendwann bei 100km/h sein.");
+            }
+        }
+    }
+}
