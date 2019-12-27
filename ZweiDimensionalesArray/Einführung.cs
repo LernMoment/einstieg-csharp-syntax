5b69dde78891295f0231fc3abd5c66309507e92d
Jan Suchotzki <jan@lernmoment.de>
2019-12-27 18:31:29 +0100

    Fügt Beispiel für 2dArray hinzu

---
+++ ZweiDimensionalesArray/Einführung.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZweiDimensionalesArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Zwei-Dimensionales Array (3x3 - wie Telefontastatur)");
+            int[,] tabelle = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+
+            for (int spaltenIndex = 0; spaltenIndex < 3; spaltenIndex++)
+            {
+                for (int zeilenIndex = 0; zeilenIndex < 3; zeilenIndex++)
+                {
+                    Console.WriteLine($"Spalte: {spaltenIndex}, Zeile: {zeilenIndex}, Inhalt: {tabelle[spaltenIndex, zeilenIndex]}");
+                }
+            }
+
+            Console.WriteLine("Zum beenden bitte RETURN drücken!");
+            Console.ReadLine();
+        }
+    }
+}
